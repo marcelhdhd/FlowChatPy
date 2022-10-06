53113f858c36d5b82746c6ea9e14ba61c393461b
Chefmaks <maximiliankratzer@gmail.com>
2022-10-06 19:00:04 +0200

    Added Comments for Code

--- Network/send.py
+++ Network/send.py
@@ -1,10 +1,9 @@
 import socket
 
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)             # defines type of socket
 
-host = "localhost" # "127.0.1.1"
-port = 8000
+host = "localhost"                                                        # set this to receipiants address (from Core?)
+port = 8000                                                               # port to send to (anything > 1023 is fine)
 
-my_socket.connect((host, port))
-my_socket.send("hello".encode()) #Fill Text with User Input from GUImanager @TODO
-#Test123
+my_socket.connect((host, port))                                           # connects to previously specified receiver
+my_socket.send("hello".encode())                                          # fill Text with User Input from GUImanager (sends encoded message)
