4a94919d9f94e7a685979e461a1d74d83cb931c2
marcelhdhd <marcel.popp@gmail.com>
2023-01-10 11:30:07 +0100

    added bottom scrollbar to scroll past longer messages

--- GUI/guimanager.py
+++ GUI/guimanager.py
@@ -5,6 +5,7 @@ from tkinter import messagebox
 from datetime import datetime
 
 import Network.networkmanager
+import guimanager
 
 
 # In-depth tutorial for tkinter
@@ -30,12 +31,15 @@ class Guimanager:
         # Here are all widgets defined#
         ###############################
         # scrollbar to scroll to past messages |TODO: autoscrolling
+        # scrollbar at the bottom to view large messages TODO: make both scrollbars work
         self.scrollbar = Scrollbar(self.messages_frame)
+        self.scrollbar_bottom = Scrollbar(self.messages_frame, orient='horizontal')
 
         # message to be sent | StringVar() helps with entry | TODO: look into StringVar more
         self.my_msg = StringVar()
-        # message-box
-        self.msg_box = Listbox(self.messages_frame, height=15, width=50, yscrollcommand=self.scrollbar.set)
+        # message-box with scrollbars
+        self.msg_box = Listbox(self.messages_frame, height=15, width=50)
+        self.msg_box.config(yscrollcommand=self.scrollbar.set, xscrollcommand=self.scrollbar_bottom.set)
         # entry-box. What you write there becomes my_msg
         self.entry_box = Entry(self.gui, width=45, textvariable=self.my_msg)
         # send-button
@@ -44,7 +48,9 @@ class Guimanager:
         # Here are all widgets placed #
         ###############################
         # Places the scrollbar to the right, stretching on the Y-axis
+        # Also places additional bar to the bottom, stretching on the X-Axis
         self.scrollbar.pack(side=RIGHT, fill=Y)
+        self.scrollbar_bottom.pack(side=BOTTOM, fill=X)
         # Places the messagebox to the left | TODO: look into stretching when making the window bigger
         self.msg_box.pack(side=LEFT)
 
@@ -61,6 +67,8 @@ class Guimanager:
         # Recieve new messages as a new thread
         recv = threading.Thread(target=self.poll_for_new_messages)
         recv.start()
+
+        self.gui.wm_attributes("-topmost", 1)
         # Start mainloop
         self.gui.mainloop()
 
@@ -68,9 +76,11 @@ class Guimanager:
     def on_closing(self, *args):
         # Asks if you really want to quit TODO: send message, that the user is leaving
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
-            # Close
+            # Close Window
             self.gui.destroy()
+            # Close sockets
             Network.networkmanager.on_closing()
+            # Quit all threads
             os._exit(0)
 
     # sends message to other users and empties send box
@@ -83,12 +93,17 @@ class Guimanager:
     # poll for new messages in networkmanager and push them to Chat box
     def poll_for_new_messages(self):
         while True:
+            # In case new messages are found in the message_queue:
             if Network.networkmanager.message_queue:
                 for tuplemsg in Network.networkmanager.message_queue:
+                    # Save ip and message to variable
                     self.ip, self.msg = tuplemsg
+                    # Get current time and format message
                     self.current_time = datetime.now().strftime("[%H:%M:%S] ")
                     self.message = self.current_time + self.ip + " : " + self.msg
+                    # Push message to message box
                     self.msg_box.insert(END, self.message)
+                    # remove message from message_queue
                     Network.networkmanager.message_queue.remove(tuplemsg)
                     # Scrolled automatisch zu einer neuen Nachricht
                     self.msg_box.see("end")
