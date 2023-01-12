455b4a6b7c38aa57e2cff35ff69d78b0ced4481c
Hormeß <jonas.hormess@web.de>
2023-01-12 13:26:52 +0100

    gui test commit for up-to-date

--- Core/core.py
+++ Core/core.py
@@ -1,4 +1,4 @@
-import user
+import fuser
 import os
 import threading
 import time
@@ -9,11 +9,19 @@ from datetime import datetime
 
 import Network.networkmanager
 
-user = fuser.FlowUser("127.0.0.1", "FlowUser")  # Test Constructor for the 'FlowUser'
+user = fuser.FlowUser(Network.networkmanager.ip_finder(), "FlowUser")  # Test Constructor for the 'FlowUser'
 
 print(user.ip)
 print(user.name)
-print(user.id)
+
+
+class ChangeNameGui:
+
+    def __init__(self):
+        self.container = Tk()
+
+        self.container.title("Change name")
+        self.container.geometry("300x200")
 
 
 class CoreTestUI:
@@ -27,23 +35,25 @@ class CoreTestUI:
         self.container.geometry("400x300")
         # Menu bar
         self.menu_bar_main = Menu(self.container)
+        # Menu bar Main tab
         self.menu_bar_start = Menu(self.menu_bar_main, tearoff=0)
         self.menu_bar_start.add_cascade(label="upper")
         self.menu_bar_start.add_separator()
-        self.menu_bar_start.add_command(label="Beenden", command=self.on_closing)
-        self.menu_bar_start.add_cascade(label="lower")
-        self.menu_bar_test1 = Menu(self.menu_bar_main, tearoff=0)
-        self.menu_bar_test1.add_cascade(label="upper")
-        self.menu_bar_start.add_separator()
-        self.menu_bar_test1.add_command(label="lower")
+        self.menu_bar_start.add_command(label="Close", command=self.on_closing)
+        # Menu bar Settings tab
+        self.menu_bar_settings = Menu(self.menu_bar_main, tearoff=0)
+        self.menu_bar_settings.add_cascade(label="Change Name")
+        self.menu_bar_settings.add_separator()
+        self.menu_bar_settings.add_command(label="lower")
+        # Menu bar Others tab
         self.menu_bar_test2 = Menu(self.menu_bar_main)
         self.menu_bar_test2.add_cascade(label="test")
         self.menu_bar_test2.add_command(label="test2")
 
         self.menu_bar_main.add_cascade(label="Start", menu=self.menu_bar_start)
-        self.menu_bar_main.add_cascade(label="test1", menu=self.menu_bar_test1)
+        self.menu_bar_main.add_cascade(label="Settings", menu=self.menu_bar_settings)
         self.menu_bar_main.add_cascade(label="test2", menu=self.menu_bar_test2)
-        self.menu_bar_main.add_command(label="Beenden", command=self.on_closing)
+        self.menu_bar_main.add_command(label="Close", command=self.on_closing)
 
         self.container.config(menu=self.menu_bar_main)
         # Grid configuration
@@ -53,9 +63,10 @@ class CoreTestUI:
         self.container.rowconfigure(1, weight=0, uniform="column")
         # Widget initialization
         self.widget_scrollbar = Scrollbar(self.container)
-        self.widget_scrollbar.grid(row=0, rowspan=1, column=0, columnspan=1, sticky="ns")
+        self.widget_scrollbar.grid(sticky="nesw")
         self.widget_msg_box = Listbox(self.container, height=15, width=50, yscrollcommand=self.widget_scrollbar.set)
         self.widget_msg_box.grid(row=0, rowspan=1, column=0, columnspan=1, sticky="nsew")
+
         self.widget_my_msg = StringVar()
         self.widget_entry_box = Entry(self.container, width=45, textvariable=self.widget_my_msg)
         self.widget_entry_box.bind("<Return>", self.do_send)
@@ -63,9 +74,11 @@ class CoreTestUI:
         self.widget_button_send = Button(self.container, text="Send", command=self.do_send)
         self.widget_button_send.grid(row=1, column=1)
 
+        self.message_poll_state = True
+
         # Recieve new messages as a new thread
-        recv = threading.Thread(target=self.poll_for_new_messages)
-        recv.start()
+        self.recv = threading.Thread(target=self.poll_for_new_messages)
+        self.recv.start()
         # Start mainloop
         self.container.mainloop()
 
@@ -74,16 +87,21 @@ class CoreTestUI:
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
             # Close
             self.container.destroy()
+            self.message_poll_state = False
             Network.networkmanager.on_closing()
             os._exit(0)
 
     def do_send(self, *args):
+        if len(args) == 0:
+            return
         # Deletes what you wrote in the entry-box
         Network.networkmanager.send_message(self.widget_my_msg.get())
         self.widget_my_msg.set("")
 
     def poll_for_new_messages(self):
-        while True:
+        while self.message_poll_state:
+            if not self.message_poll_state:
+                break
             time.sleep(0.1)
             if Network.networkmanager.message_queue:
                 for tuplemsg in Network.networkmanager.message_queue:
---
+++ Core/fuser.py
@@ -0,0 +1,7 @@
+# todo consider renaming fuser to flowchatuser or user to be less ambiguous
+
+class FlowUser:                         # Constructor for the User Part
+
+    def __init__(self, ip, name):
+        self.ip = ip
+        self.name = name
