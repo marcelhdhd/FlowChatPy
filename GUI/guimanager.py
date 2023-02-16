9e330031a23984f68683923e093a67a50c83a79e
marcelhdhd <marcel.popp@aikq.de>
2023-02-16 15:31:35 +0100

    changed button to display "confirm" changename window now on top made it so pressing confirm would close the popped up windows again

--- GUI/guimanager.py
+++ GUI/guimanager.py
@@ -16,15 +16,17 @@ class NameChangeWindow(CTkToplevel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title("Change name")
+        self.attributes('-topmost', True)
         self.testname = StringVar()
         self.nameentrywidget = CTkEntry(self, textvariable=self.testname)
-        self.changenameButton = CTkButton(self, command=self.changeName)
+        self.changenameButton = CTkButton(self, text="Confirm", command=self.changeName)
         self.nameentrywidget.pack()
         self.changenameButton.pack()
 
     def changeName(self):
         print("nameentrywidget.get() = " + self.nameentrywidget.get())
         Network.networkmanager.username = self.nameentrywidget.get()
+        self.destroy()
 
 
 # In-depth tutorial for tkinter
