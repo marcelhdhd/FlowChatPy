c4c5985f497f31513077fc64adc9a619a62787ee
Pascal Mika <pascl.mika@t-online.de>
2023-02-28 08:33:30 +0100

    refactored

--- GUI/guimanager.py
+++ GUI/guimanager.py
@@ -10,23 +10,7 @@ import customtkinter
 from customtkinter import *
 
 import Network.networkmanager
-
-
-class NameChangeWindow(CTkToplevel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.title("Change name")
-        self.attributes('-topmost', True)
-        self.testname = StringVar()
-        self.nameentrywidget = CTkEntry(self, textvariable=self.testname)
-        self.changenameButton = CTkButton(self, text="Confirm", command=self.changeName)
-        self.nameentrywidget.pack()
-        self.changenameButton.pack()
-
-    def changeName(self):
-        print("nameentrywidget.get() = " + self.nameentrywidget.get())
-        Network.networkmanager.username = self.nameentrywidget.get()
-        self.destroy()
+from changename import NameChangeWindow
 
 
 # In-depth tutorial for tkinter
