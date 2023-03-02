5ff09e8c5f23e85aee6932d7824595d6c0894a77
marcelhdhd <marcel.popp@aikq.de>
2023-03-02 08:28:06 +0100

    namechangeWindow window manager now hidden

--- GUI/guimanager.py
+++ GUI/guimanager.py
@@ -12,14 +12,18 @@ class NameChangeWindow(CTkToplevel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title("Change name")
+        # title not showing due to window being to small
+        # maybe disable window buttons and add close button to popup
         self.attributes('-topmost', True)
+        self.overrideredirect(True)
         self.testname = StringVar()
         self.nameentrywidget = CTkEntry(self, textvariable=self.testname)
-        self.changenameButton = CTkButton(self, text="Confirm", command=self.changeName)
+        self.changenameButton = CTkButton(self, text="Change name", command=self.changeName)
+        self.nameentrywidget.bind("<Return>", self.changeName)
         self.nameentrywidget.pack()
         self.changenameButton.pack()
 
-    def changeName(self):
+    def changeName(self, *args):
         print("nameentrywidget.get() = " + self.nameentrywidget.get())
         Network.networkmanager.username = self.nameentrywidget.get()
         self.destroy()
